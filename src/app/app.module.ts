45073760784e3c550b885405fa0b40fb5b42caf3
Hans <batubarahans299@gmail.com>
2025-04-01 00:09:38 +0700

    progress hari ini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Tugas2Component } from './pages/tugas2/tugas2.component';
+import { StatusTransformPipe } from './pipe/status-transform.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Tugas2Component
+    Tugas2Component,
+    StatusTransformPipe
   ],
   imports: [
     BrowserModule,
