9f058f912f0c9b9c2e2f6c0bcd8d3286902e5ac6
Hans <batubarahans299@gmail.com>
2025-04-01 21:53:23 +0700

    progress hari ini

--- src/app/pages/tugas2/tugas2.component.ts
+++ src/app/pages/tugas2/tugas2.component.ts
@@ -11,62 +11,52 @@ export class Tugas2Component {
       nama:'Andi',
       semester:'1',
       MataKuliah:'Matematika',
-      status:['Aktif']
     },
     {
       nama:'Budi',
       semester:'2',
       MataKuliah:'Fisika',
-      status:['Izin']
     },
     {
       nama:'Siti',
       semester:'3',
       MataKuliah:'Ekonomi',
-      status:['Aktif']
     },
     {
       nama:'Rina',
       semester:'4',
       MataKuliah:'Biologi',
-      status:['DO']
     },
     {
       nama:'Dani ',
       semester:'5',
       MataKuliah:'Sosiologi',
-      status:['Aktif']
     },
     {
       nama:'Joko',
       semester:'6',
       MataKuliah:'Kimia',
-      status:['Izin']
     },
     {
       nama:'Lilis',
       semester:'7',
       MataKuliah:'Sejarah',
-      status:['Aktif']
     },
     {
       nama:'Rudi',
       semester:'8',
       MataKuliah:'Seni',
-      status:['Do']
     },
     {
       nama:'Tini',
       semester:'9',
       MataKuliah:'Geografi',
-      status:['Aktif']
     },
     {
       nama:'Wawan',
       semester:'10',
       MataKuliah:'Sidang',
-      status:['Aktif']
     },
   ]
-
+  status:string='Aktif'
 }
